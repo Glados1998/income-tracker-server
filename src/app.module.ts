50d0ebce96fb7f890fa36792e03a569b0b0970f4
jerome greder <j.greder@staro.ch>
2024-08-02 17:21:04 +0200

    Add modules for handling expenses and incomes
    This commit introduces the Expenses and Incomes modules, including their respective controllers, services, and DTOs. It also adds the `@nestjs/mapped-types` dependency to support DTO inheritance. These modules provide endpoints for creating, reading, updating, and deleting expenses and incomes.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IncomesModule } from './incomes/incomes.module';
+import { ExpensesModule } from './expenses/expenses.module';
 
 @Module({
-  imports: [],
+  imports: [IncomesModule, ExpensesModule],
   controllers: [AppController],
   providers: [AppService],
 })
