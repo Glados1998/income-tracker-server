0efa332a2ff0a0d1211c81562486a01bafc023fa
jerome greder <j.greder@staro.ch>
2024-08-03 22:40:39 +0200

    Add authentication module with AuthController and AuthService
    Created a new AuthModule for handling authentication. Added AuthController and AuthService with corresponding tests to ensure they are defined. Integrated AuthModule into the main application module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IncomesModule } from './incomes/incomes.module';
 import { ExpensesModule } from './expenses/expenses.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [IncomesModule, ExpensesModule],
+  imports: [IncomesModule, ExpensesModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
