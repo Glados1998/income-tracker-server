c9c74907213554798869a753958ff609cc9ce54d
jerome greder <j.greder@staro.ch>
2024-08-04 02:09:33 +0200

    Add TypeORM integration and update dependencies
    This commit integrates TypeORM with PostgreSQL into the application, configuring it with necessary details in `app.module.ts`. Additionally, various dependencies are updated in `package-lock.json`, marking several dev dependencies as optional.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,21 @@ import { AppService } from './app.service';
 import { IncomesModule } from './incomes/incomes.module';
 import { ExpensesModule } from './expenses/expenses.module';
 import { AuthModule } from './auth/auth.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [IncomesModule, ExpensesModule, AuthModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'root',
+    database: 'budget_app',
+    entities: [__dirname + '/**/*.entity.ts'],
+    synchronize: true,
+    logging: true,
+  }),
+      IncomesModule, ExpensesModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
